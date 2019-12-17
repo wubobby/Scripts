2221907f67d9d4daae5eb0352acdf11cc9c8d84e
bobby <bobby0824@gmail.com>
2019-12-17 13:31:08 +0800

    userloginlog

--- oneweeklog.sh
+++ oneweeklog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 LIST=$(mysql -u$USER -p$PWD $HOST -e "select user_id from pairs where role_id=3;" | grep -v user > /tmp/A )
-
+DATES=$(date '+%Y%m%d')
+DATEE=$(date --date='8 days ago' "+%Y%m%d")
 
 filename="/tmp/A"
 exec < $filename
@@ -10,9 +11,11 @@ while read line
 do
    NAME=$(mysql -u$USER -p$PWD $HOST -e "select user_name from users where user_id=$line;" | grep -v user_name)
    TIMES=$(mysql -u$USER -p$PWD $HOST -e "select count(*) from userLogs where level='info' and action='login' and action_time> 'between current_date()-7 and sysdate()' and user_id=$line;" | grep -v count)
-   echo "NAME=$NAME  TIMES=$TIMES"
- 
+   echo "NAME=$NAME  TIMES=$TIMES" >> /tmp/B
 done
-
-
-
+sort -r -n  -t, -k2 /tmp/B > /LogFile-$(date '+%Y%m%d').csv
+rm -f /tmp/B
+sed -i "1iuser logins per week" /LogFile-$(date '+%Y%m%d').csv
+sed -i "2iHostName:'$(hostname)'" /LogFile-$(date '+%Y%m%d').csv
+sed -i "3iDate range:$DATEE <-> $DATES"  /LogFile-$(date '+%Y%m%d').csv
+sed -i "4iAccount,Count" /LogFile-$(date '+%Y%m%d').csv
